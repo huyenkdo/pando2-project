9453e22a18f8f1b23d7f3ecfa05c0b5b6c6c9fae
Huyen Do <huyenkdo@gmail.com>
2024-09-03 00:55:19 +0200

    did the front

--- app.rb
+++ app.rb
@@ -6,6 +6,11 @@ require_relative 'models/measurement'
 get "/" do
   @measurements = Measurement.all
   @co2_measurements = @measurements.select { |m| m.measure_type == 'CO2' }
+  @tmp_measurements = @measurements.select { |m| m.measure_type == 'TMP' }
+  @hum_measurements = @measurements.select { |m| m.measure_type == 'HUM' }
+  @voct_measurements = @measurements.select { |m| m.measure_type == 'VOCT' }
+  @room_names_fr = @measurements.map { |m| m.room_name.sub('Room', 'Salle') }.uniq
+  @types = @measurements.map(&:measure_type).uniq
 
   @test = @measurements.first.timestamp.class
   erb :home
